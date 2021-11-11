3acb356fa2607aecf580bbcd1a644bac1c419f3b
micjlee <72898666+micjlee@users.noreply.github.com>
2021-11-10 23:02:07 -0600

    Added second brain, links to dictionary

--- functions/dashboard.py
+++ functions/dashboard.py
@@ -26,13 +26,14 @@ from dash import dcc
 import brain_visual3 as b3
 import Create_Slider_fn as cs
 import data_processing as dp
+import highest_electrodes as he
 import pandas as pd
 import plotly.graph_objs as go
 
 
 # function that returns a figure with the biometric graph
 # INPUTS - the dataframe and biometric to be graphed - string, i.e HR or SpO2, and epochbm dict
-# OUTPUTS - the graph 
+# OUTPUTS - the graph
 def graph_biometric(df, biometric, epochbm_dict):
     df_hr = df[biometric]
     df_hr_epoch_1 = df_hr[epochbm_dict['1'][0][0]:epochbm_dict['1'][0][1]]
@@ -40,7 +41,7 @@ def graph_biometric(df, biometric, epochbm_dict):
 
     fig = go.Figure(data=[go.Scatter(x=list(range(len(hr_epoch))), y=hr_epoch)])
     fig.update_layout(
-        title="%s for Epoch 1" %(biometric),
+        title="%s for Epoch 1" % biometric,
         title_x=0.5,
         xaxis_title="Time",
         yaxis_title=biometric,
@@ -54,25 +55,31 @@ def graph_biometric(df, biometric, epochbm_dict):
 # INPUTS - the dataframe and biometric to be graphed - string, i.e HR or SpO2, and epochbm dict
 #           and epoch as an int
 # OUTPUTS - the figure
-def graph_biometric_selection(df, biometric, epochbm_dict, epoch):
+def graph_biometric_selection(biometric, epochbm_dict, epoch):
     epoch = str(epoch)
-    df_hr = df[biometric]
-    df_hr_epoch_x = df_hr[epochbm_dict[epoch][0][0]:epochbm_dict[epoch][0][1]]
-    hr_epoch = df_hr_epoch_x.to_numpy()
 
-    fig = go.Figure(data=[go.Scatter(x=list(range(len(hr_epoch))), y=hr_epoch)])
+    # Get the x values from the dictionary,
+    #   numerate from each endpoint,
+    #   and convert to seconds by dividing by 500
+    x_values = epochbm_dict[epoch][0]
+    x_values = list(range(x_values[0], x_values[1]))
+    x_values = [number / 500 for number in x_values]
+
+    y_df = epochbm_dict[epoch][2][biometric]
+    y_values = y_df.values.tolist()
+
+    fig = go.Figure(data=[go.Scatter(x=x_values, y=y_values)])
     fig.update_layout(
         title="%s for Epoch %s" % (biometric, epoch),
         title_x=0.5,
-        xaxis_title="Time",
+        xaxis_title="Time (sec)",
         yaxis_title=biometric
     )
     return fig
 
-
 def interactive_dashboard():
-    df1 = pd.read_csv('./assets/data/sampleDataBM.csv')  # dummy data for now
-    df1 = df1.dropna()
+    # df1 = pd.read_csv('./assets/data/sampleDataBM.csv')  # dummy data for now
+    # df1 = df1.dropna()
 
     # define input vhdr filename
     vhdr_fname = './assets/data/2020_06_04_T05_U00T_EEG01/2020_06_04_T05_U00T_EEG01.vhdr'
@@ -83,16 +90,6 @@ def interactive_dashboard():
     # perform data processing: get data dictionary from raw data
     epochbm_dict = dp.data_processing(vhdr_fname, var_name, num_bins)
 
-    # Secondary component that determines what coordinate to plot
-    mycheckbox = dcc.Checklist(
-        id='checklist-for-graph',
-        options=[
-            {'label': 'Fp1', 'value': 'Fp1'},
-            {'label': 'Fp2', 'value': 'Fp2'},
-            {'label': 'F3', 'value': 'F3'},
-        ],
-        value=['Fp1', 'Fp2', 'F3']
-    )
     # get slider with epochs
     slider = cs.create_slider(epochbm_dict)
     # Filepath of nii file from assets folder in the same directory
@@ -101,7 +98,7 @@ def interactive_dashboard():
     filepath_name = 'assets/EEG01_chanlocs_cartesian.txt'
 
     # get visual parts
-    data_brain, df, brain_fig = b3.brain_visual3(img_file, filepath_name, mycheckbox, 'value')
+    data_brain, df = b3.brain_visual3(img_file, filepath_name)
 
     # external stylesheet for format
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -111,36 +108,52 @@ def interactive_dashboard():
         html.Div([  # upper row with brain and hr plot
             html.Div([
                 html.Div([
-                    mycheckbox,
-                ]),
-                html.Div([
                     dcc.Graph(
-                        id="image-display-graph-3d",
-                        config=dict(displayModeBar=False),
-                        # figure=fig
-                    ),
+                        id="image-display-graph-3d-1",
+                        config=dict(displayModeBar=False)
+                    )
                 ]),
             ], className='six columns'),
             html.Div([
                 dcc.Graph(id='hr-graph1')
             ], className='six columns'),
         ], className='row'),
-        html.Div([  # bottom row with just spo2 plot for now
+
+        html.Div([  # bottom row
+            html.Div([
+                html.Div([
+                    dcc.Graph(
+                        id="image-display-graph-3d-2",
+                        config=dict(displayModeBar=False),
+                    )
+                ])
+            ], className='six columns'),
             html.Div([
                 dcc.Graph(id='SpO2-graph1')
-            ], className='six columns'),
+            ], className='six columns')
         ], className='row'),
 
         slider
     ])
 
-    # Updates 3d brain when callback_object1 is changed
+    # Updates 3d brain 1 (Theta band) with the electrode to plot when slider is changed
     @app.callback(
-        dash.dependencies.Output('image-display-graph-3d', 'figure'),
-        dash.dependencies.Input(mycheckbox.id, 'value')
+        dash.dependencies.Output('image-display-graph-3d-1', 'figure'),
+        dash.dependencies.Input(slider.id, 'value')  # for the electrode value
     )
     def update_graph(selected_value):
-        fig = b3.make_3d_fig(data_brain, df, selected_value)
+        high3 = he.highest_electrodes(epochbm_dict, selected_value, "Theta")
+        fig = b3.make_3d_fig(data_brain, df, high3, "Theta")
+        return fig
+
+    # Updates 3d brain 2 (Alpha band) with the electrode to plot when slider is changed
+    @app.callback(
+        dash.dependencies.Output('image-display-graph-3d-2', 'figure'),
+        dash.dependencies.Input(slider.id, 'value')  # for the electrode value
+    )
+    def update_graph(selected_value):
+        high3 = he.highest_electrodes(epochbm_dict, selected_value, "Alpha")
+        fig = b3.make_3d_fig(data_brain, df, high3, "Alpha")
         return fig
 
     # Updates hr plot when the slider is changed
@@ -149,7 +162,7 @@ def interactive_dashboard():
         dash.dependencies.Input(slider.id, 'value')
     )
     def update_graph1(selected_value):
-        fig = graph_biometric_selection(df1, 'HR', epochbm_dict, selected_value)
+        fig = graph_biometric_selection('HR', epochbm_dict, selected_value)
         return fig
 
     # Updates SpO2 plot when the slider is changed
@@ -158,7 +171,7 @@ def interactive_dashboard():
         dash.dependencies.Input(slider.id, 'value')
     )
     def update_graph1(selected_value):
-        fig = graph_biometric_selection(df1, 'SpO2', epochbm_dict, selected_value)
+        fig = graph_biometric_selection('SpO2', epochbm_dict, selected_value)
         return fig
 
     app.run_server(debug=True)
